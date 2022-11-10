de754f477044fb8099f732e81e6408100104fe25
Jan Wilczek <janekw@vp.pl>
2022-11-10 07:14:00 +0100

    Simplification.

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 def main():
-    network = SimpleRNN(input_size=1, output_size=1, unit_type="LSTM", hidden_size=8, skip=0, bias_fl=False)
-
     if torch.cuda.is_available():
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
-        torch.cuda.set_device(0)
-        network = network.cuda()
         
-    optimizer = torch.optim.Adam(network.parameters(), lr=0.001, weight_decay=1e-4)
-    loss = ESRLoss()
     dataset = DataSet(data_dir='data')
     
     for subset_name, frame_len in zip(['train', 'validation', 'test'], [22050, 0, 0]):
@@ -26,12 +20,15 @@ def main():
 
     EPOCH_COUNT = 20
     VALIDATE_EVERY = 5
-    BATCH_SIZE = 64
-    INIT_LEN = 1000
-    UP_FR = 2048
-    VAL_CHUNK = 100000
+    MINIBATCH_SIZE = 64
+    SAMPLES_BETWEEN_UPDATES = 2048
+    TEST_CHUNK = 100000
     BEST_MODEL_PATH = 'best_model.pth'
     RUNS_DIRECTORY = 'runs'
+    
+    network = SimpleRNN(input_size=1, output_size=1, unit_type="LSTM", hidden_size=8, skip=0, bias_fl=False)
+    loss = ESRLoss()
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.001, weight_decay=1e-4)
     
     logger = SummaryWriter(RUNS_DIRECTORY)
     
@@ -39,14 +36,14 @@ def main():
     
     for epoch in range(1, EPOCH_COUNT + 1):
         epoch_loss = network.train_epoch(dataset.subsets['train'].data['input'][0],
-                                        dataset.subsets['train'].data['target'][0], loss, optimizer, BATCH_SIZE, INIT_LEN, UP_FR)
+                                        dataset.subsets['train'].data['target'][0], loss, optimizer, bs=MINIBATCH_SIZE, up_fr=SAMPLES_BETWEEN_UPDATES)
                                         
         print(f"Epoch {epoch}/{EPOCH_COUNT} training loss: {epoch_loss}")
         logger.add_scalar('Loss/train', epoch_loss, epoch)
         
         if epoch % VALIDATE_EVERY == 0:
             validation_output, validation_loss = network.process_data(dataset.subsets['validation'].data['input'][0],
-                                        dataset.subsets['validation'].data['target'][0], loss, VAL_CHUNK)
+                                        dataset.subsets['validation'].data['target'][0], loss, TEST_CHUNK)
 
             print(f"Validation loss: {validation_loss}")
             logger.add_scalar('Loss/validation', validation_loss, epoch)
@@ -62,7 +59,7 @@ def main():
     network.load_state_dict(best_validation_model_state['model_state_dict'])
 
     test_output, test_loss = network.process_data(dataset.subsets['test'].data['input'][0],
-                                                  dataset.subsets['test'].data['target'][0], loss, VAL_CHUNK)
+                                                  dataset.subsets['test'].data['target'][0], loss, TEST_CHUNK)
     
     print(f"Test loss: {test_loss}")
     logger.add_scalar('Loss/test', test_loss, EPOCH_COUNT)
